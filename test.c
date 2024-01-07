1bfad25d1650e8146b3fba5d570c487743e9d85d
vladfdp <vlad.heintz@gmail.com>
2024-01-07 19:03:42 +0100

    désolé pour la betise

---
+++ test.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "Fp.h"
+#include "Fp2.h"
+#include "Fp6.h"
+#include "Fp12.h"
+#include "int256.h"
+#include "int512.h"
+  
+  
+
+int main(){
+
+    uint64_t f = 0xFFFFFFFFFFFFFFFF;
+  
+    Fp2 A2 = {A1, B1}; 
+    Fp2 B2 = {B1, C1}; 
+    Fp2 C2 = Fp2_add(A2,B2);
+    Fp2 E2 = Fp2_sub(A2,B2);
+    Fp2 F2 = Fp2_mul(A2,B2);
+
+
+    print_Fp2(A2);printf("\n");
+    print_Fp2(B2);printf("\n");
+    print_Fp2(C2);printf("\n");
+    print_Fp2(E2);printf("\n");
+    print_Fp2(F2);printf("\n"); printf("\n");
+
+    Fp6 A3 = {A2,B2,C2};
+    Fp6 B3 = {C2,C2,A2};
+    Fp6 C3 = Fp6_add(A3,B3);
+    Fp6 D3 = Fp6_sub(A3,B3);
+    Fp6 E3 = Fp6_mul(A3,B3);
+
+    print_Fp6(A3);printf("\n");
+    print_Fp6(B3);printf("\n");
+    print_Fp6(C3);printf("\n");
+    print_Fp6(D3);printf("\n");
+    print_Fp6(E3);printf("\n"); printf("\n");
+
+
+
+
+    Fp12 A4 = {A3, B3};
+    Fp12 B4 = {C3, A3};
+    Fp12 C4 = Fp12_add(A4,B4);
+    Fp12 D4 = Fp12_mul(A4,B4);
+    Fp12 E4 = Fp12_sub(A4,B4);
+
+    print_Fp12(A4);printf("\n");
+    print_Fp12(B4);printf("\n");
+    print_Fp12(C4);printf("\n");
+    print_Fp12(D4);printf("\n");
+    print_Fp12(E4);printf("\n");
+
+
+    return 0;
+}
